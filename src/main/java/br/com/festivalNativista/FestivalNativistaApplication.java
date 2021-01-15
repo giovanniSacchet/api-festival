26ccd080effbddf75cf574646c07e39ea106e542
Giovanni Sacchet <giovanni_xs@hotmail.com>
2021-01-15 17:39:34 -0300

    start

---
+++ src/main/java/br/com/festivalNativista/FestivalNativistaApplication.java
@@ -0,0 +1,13 @@
+package br.com.festivalNativista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalNativistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalNativistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/festivalNativista/FestivalNativistaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.festivalNativista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalNativistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

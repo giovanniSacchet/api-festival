79ea80b93dcea47f992f36b920e90527c77f7c79
Giovanni Sacchet <giovanni_xs@hotmail.com>
2021-01-21 18:18:32 -0300

    cross nos controller

--- src/main/java/br/com/festivalNativista/controller/EmailController.java
+++ src/main/java/br/com/festivalNativista/controller/EmailController.java
@@ -5,14 +5,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.festivalNativista.form.EmailForm;
 import br.com.festivalNativista.service.EmailService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/email")
 public class EmailController {

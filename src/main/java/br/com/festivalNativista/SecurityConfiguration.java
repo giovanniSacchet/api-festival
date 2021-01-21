c27f21a51a9abc8bc568d3913e59c31afa8f5446
Giovanni Sacchet <giovanni_xs@hotmail.com>
2021-01-21 19:13:48 -0300

    alteração cors

--- src/main/java/br/com/festivalNativista/SecurityConfiguration.java
+++ src/main/java/br/com/festivalNativista/SecurityConfiguration.java
@@ -11,14 +11,17 @@ public class SecurityConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                		.allowedOrigins("http://localhost:3000")
-                        .allowedMethods("POST");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                		.allowedOrigins("*");
 //                        .allowedMethods("GET", "POST", "PUT", "DELETE")
 //                        .allowedHeaders("*")
             }
+
         };
     }
 }
